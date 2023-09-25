6d13851b3b44e4171b2f0e227df835e02be55bc7
AwalHossain <waqin21@gmail.com>
2023-09-25 22:38:09 +0600

    fix: changed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Provider from '@/lib/Provider'
+import Providers from '@/lib/Provider'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <Provider>
+    <Providers>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
-    </Provider>
+    </Providers>
   )
 }
