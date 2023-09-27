8fe12938c0b583abde5b940c78767d2568d35bd4
AwalHossain <waqin21@gmail.com>
2023-09-27 10:25:55 +0600

    feat: Add resoponsive sidebar 🗒️

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import { USER_ROLE } from '@/constants/role';
+import { SidebarItems } from '@/constants/SiderbarItems';
+
 import { Layout, Menu } from "antd";
 import { useState } from "react";
 
@@ -19,34 +14,8 @@ const { Sider } = Layout;
 const Sidebar = () => {
 
     const [collapsed, setCollapsed] = useState(false);
-    type MenuItem = Required<MenuProps>['items'][number];
 
-    function getItem(
-        label: React.ReactNode,
-        key: React.Key,
-        icon?: React.ReactNode,
-        children?: MenuItem[],
-    ): MenuItem {
-        return {
-            key,
-            icon,
-            children,
-            label,
-        } as MenuItem;
-    }
-
-    const items: MenuItem[] = [
-        getItem('Option 1', '1', <PieChartOutlined />),
-        getItem('Option 2', '2', <DesktopOutlined />),
-        getItem('User', 'sub1', <UserOutlined />, [
-            getItem('Tom', '3'),
-            getItem('Bill', '4'),
-            getItem('Alex', '5'),
-        ]),
-        getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-        getItem('Files', '9', <FileOutlined />),
-    ];
-
+    const role = USER_ROLE.STUDENT;
 
     return (
         <Sider collapsible
@@ -75,7 +44,7 @@ const Sidebar = () => {
             >
                 Awal-University
             </div>
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={SidebarItems(role)} />
         </Sider>
     )
 }
