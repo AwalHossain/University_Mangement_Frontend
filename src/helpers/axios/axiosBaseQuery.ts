72668d151695b8ef103f8b74e299129cc93973a9
AwalHossain <waqin21@gmail.com>
2023-09-28 15:28:31 +0600

    feat: Add axiosBaseQuery instead built-in redux fetch 🔀

---
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -0,0 +1,42 @@
+import { IMeta } from '@/types'
+import type { BaseQueryFn } from '@reduxjs/toolkit/query'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+
+export const axiosBaseQuery =
+    (
+        { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+    ): BaseQueryFn<
+        {
+            url: string
+            method: AxiosRequestConfig['method']
+            data?: AxiosRequestConfig['data']
+            params?: AxiosRequestConfig['params']
+            meta?: IMeta
+            ContentType?: string
+        },
+        unknown,
+        unknown
+    > =>
+        async ({ url, method, data, params }) => {
+            try {
+                const result = await axios({
+                    url: baseUrl + url, method, data, params,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+
+                })
+                return { data: result.data }
+            } catch (axiosError) {
+                let err = axiosError as AxiosError
+                return {
+                    error: {
+                        status: err.response?.status,
+                        data: err.response?.data || err.message,
+                    },
+                }
+            }
+        }
+
+
