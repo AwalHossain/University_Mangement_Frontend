e5ee111926f5dc1be947ea5c06359c1e3a524a7b
AwalHossain <waqin21@gmail.com>
2023-09-28 15:31:21 +0600

    feat: Add baseApi for the whole api endpoint 💪

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,18 @@
+
+// Need to use the React-specific entry point to import createApi
+import { axiosBaseQuery } from '@/helpers/axios/axiosBaseQuery'
+import { baseUrl } from '@/helpers/config/envConfig'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+    reducerPath: 'api',
+    baseQuery: axiosBaseQuery({ baseUrl: baseUrl() }),
+    endpoints: () => ({
+
+    }),
+    tagTypes: ['user'],
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
