3bc8fb43c84df5f61e3ad0dec2b4c0be6d0570c9
AwalHossain <waqin21@gmail.com>
2023-09-29 21:13:57 +0600

    feat: move the whole login code into component ⚙️

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,80 +1,18 @@
-'use client';
+import LoginPage from '@/components/login/Login'
+import { Metadata } from 'next'
 
-import Form from '@/components/form/Form';
-import FormInput from '@/components/form/FormInput';
-import { useUserLoginMutation } from '@/redux/api/authApi';
-import { isLoggedIn, storeUserInfo } from '@/service/auth.service';
-import { Button, Col, Row } from 'antd';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { SubmitErrorHandler } from 'react-hook-form';
-import LoginImage from '../../assets/Login-pana.png';
 
-type FormValues = {
-    id: string;
-    password: string;
+export const metadata: Metadata = {
+    title: "UMS | Login",
+    description: "Login Page",
+    keywords: "login, login page",
 }
 
 const Login = () => {
-    const router = useRouter();
-    const [userLogin] = useUserLoginMutation();
-
-
-    const onSubmit: SubmitErrorHandler<FormValues> = async (data: any) => {
-        const res = await userLogin(data).unwrap();
-        if (res.success) {
-            storeUserInfo({ acessToken: res?.data?.accessToken })
-        }
-
-        if (isLoggedIn()) {
-            router.push('/profile')
-        }
-
-    }
-
     return (
-        <Row
-            justify={'center'}
-            align={'middle'}
-            style={{
-                height: "100vh"
-            }}
-        >
-            <Col sm={12} md={16} lg={10}>
-                <Image src={LoginImage} width={500} alt="login" />
-            </Col>
-            <Col sm={12} md={8} lg={8} >
-                <h1
-                    style={{
-                        margin: "15px 0px",
-                    }}
-                >
-                    First login your account
-                </h1>
-                <div>
-                    <Form submitHandler={onSubmit}>
-                        <div>
-                            <FormInput name="id" type="text" size="large" label="User Id" />
-                        </div>
-                        <div
-                            style={{
-                                margin: "15px 0px",
-                            }}
-                        >
-                            <FormInput
-                                name="password"
-                                type="password"
-                                size="large"
-                                label="User Password"
-                            />
-                        </div>
-                        <Button type="primary" htmlType="submit">
-                            Login
-                        </Button>
-                    </Form>
-                </div>
-            </Col>
-        </Row >
+        <>
+            <LoginPage />
+        </>
     )
 }
 
