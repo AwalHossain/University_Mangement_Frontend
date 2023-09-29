40145e74359c6ad9f8a1485d8e07da9b560f44b1
AwalHossain <waqin21@gmail.com>
2023-09-29 10:18:44 +0600

    feat: Add profile page 👨

---
+++ src/app/(withLayout)/profile/page.tsx
@@ -0,0 +1,10 @@
+
+const ProfilePage = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1 style={{ fontSize: '3rem' }}>Welcome to The Profile, Bro!</h1>
+        </div>
+    )
+}
+
+export default ProfilePage
