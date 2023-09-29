ae2c002a41b8f3fff5ec11890ad51d94715125cf
AwalHossain <waqin21@gmail.com>
2023-09-29 10:18:19 +0600

    feat: Add private route checker ✅

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -2,18 +2,41 @@
 import Contents from '@/components/ui/Contents';
 import Sidebar from '@/components/ui/Sidebar';
 import { ChildrenProps } from '@/lib/types';
-import { Layout, theme } from 'antd';
-import { useState } from 'react';
+import { isLoggedIn } from '@/service/auth.service';
+import { Layout } from 'antd';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const { Header, Content, Footer, Sider } = Layout;
 
+import { Spin } from 'antd';
 
 
 const DashboardLayout = ({ children }: ChildrenProps) => {
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+    const [loading, setLoading] = useState(false);
+
+
+    const router = useRouter();
+    const userLoggedIn = isLoggedIn();
+
+    useEffect(() => {
+        if (!userLoggedIn) {
+            router.push('/login')
+        }
+        setLoading(true);
+    }, [router])
+
+
+    // ...
+
+    if (!loading) {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <Spin size="large" />
+            </div>
+        );
+    }
+
 
     return (
         <Layout>
