061bc5f7ac60afaee9493003f167b8b2400a3e35
AwalHossain <waqin21@gmail.com>
2023-10-01 21:44:26 +0600

    refactor: datePicker modified ✔️

--- src/components/form/FormDatePicker.tsx
+++ src/components/form/FormDatePicker.tsx
@@ -1,52 +1,46 @@
-'use client'
 import { DatePicker, DatePickerProps } from "antd";
 import dayjs, { Dayjs } from "dayjs";
 import { Controller, useFormContext } from "react-hook-form";
 
-
-interface IInput {
+type UMDatePikerProps = {
     onChange?: (valOne: Dayjs | null, valTwo: string) => void;
     name: string;
-    size?: "large" | "small";
-    value?: Dayjs
     label?: string;
-
-
-}
+    value?: Dayjs;
+    size?: "large" | "small";
+};
 
 const FormDatePicker = ({
     name,
-    size = "large",
     label,
     onChange,
-}: IInput) => {
-
-
+    size = "large",
+}: UMDatePikerProps) => {
     const { control, setValue } = useFormContext();
-    const handleChange: DatePickerProps["onChange"] = (date, dateString) => {
-        if (onChange) {
-            onChange(date, dateString);
-        }
+
+    const handleOnChange: DatePickerProps["onChange"] = (date, dateString) => {
+        onChange ? onChange(date, dateString) : null;
         setValue(name, dateString);
     };
+
     return (
         <div>
             {label ? label : null}
+            <br />
             <Controller
-                control={control}
                 name={name}
+                control={control}
                 render={({ field }) => (
-                    < DatePicker
-                        size={size}
-                        style={{ width: "100%" }}
+                    <DatePicker
                         value={dayjs(field.value) || ""}
-                        onChange={handleChange}
+                        size={size}
+                        onChange={handleOnChange}
+                        style={{ width: "100%" }}
                     />
                 )}
             />
-
         </div>
-    )
-}
+    );
+};
 
 export default FormDatePicker;
