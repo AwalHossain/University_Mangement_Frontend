6e60ff2ad5b277b4218111f2eed04c03ba5f3e32
AwalHossain <waqin21@gmail.com>
2023-10-01 19:07:01 +0600

    feat: Add different options for differen select input 📝

---
+++ src/constants/global.ts
@@ -0,0 +1,80 @@
+
+
+export const genderOptions = [
+    {
+        label: "Male",
+        value: "male"
+    },
+    {
+        label: "Female",
+        value: "female"
+    },
+    {
+        label: "Others",
+        value: "others"
+    }
+]
+
+
+export const departmentOptions = [
+    {
+        label: "HR",
+        value: "hr manager"
+    },
+    {
+        label: "Finance",
+        value: "finance"
+    },
+    {
+        label: "Management",
+        value: "management"
+    },
+]
+
+
+
+export const bloodGroupOptions = [
+    {
+        label: 'A+',
+        value: 'A+',
+    },
+    {
+        label: 'A-',
+        value: 'A-',
+    },
+    {
+        label: 'B+',
+        value: 'B+',
+    },
+    {
+        label: 'B-',
+        value: 'B-',
+    },
+    {
+        label: 'AB+',
+        value: 'AB+',
+    },
+    {
+        label: 'AB-',
+        value: 'AB-',
+    },
+    {
+        label: 'O+',
+        value: 'O+',
+    },
+    {
+        label: 'O-',
+        value: 'O-',
+    },
+];
+
+export const facultyOptions = [
+    {
+        label: 'Engineering',
+        value: 'engineering',
+    },
+    {
+        label: 'Faculty of science and engineering',
+        value: 'Faculty of science and engineering',
+    },
+]
