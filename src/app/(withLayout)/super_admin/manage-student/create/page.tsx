c19bb9931589b4d89d1ef7f4aa55f7bf83251ce1
AwalHossain <waqin21@gmail.com>
2023-10-03 03:20:48 +0600

    feat: Add create-student form with stepperForm ✅

--- src/app/(withLayout)/super_admin/manage-student/create/page.tsx
+++ src/app/(withLayout)/super_admin/manage-student/create/page.tsx
@@ -1,7 +1,42 @@
-import UMbreadCrumb from "@/components/ui/UMbreadCrumb"
+'use client';
+
+import StepperForm from "@/components/stepperForm/StepperForm";
+import GuardianInfo from "@/components/studentForm/GuardianInfo";
+import LocalGuardianInfo from "@/components/studentForm/LocalGuardianInfo";
+import StudentBasicInfo from "@/components/studentForm/StudentBasicInfo";
+import StudentInfo from "@/components/studentForm/StudentInfo";
+import UMbreadCrumb from "@/components/ui/UMbreadCrumb";
 
 const CreateStuentPage = () => {
     const base = 'super_admin'
+    const steps = [
+        {
+            title: 'Student Information',
+            content: <StudentInfo />,
+        },
+        {
+            title: 'Basic Information',
+            content: <StudentBasicInfo />,
+        },
+        {
+            title: 'Guardian Information',
+            content: <GuardianInfo />,
+        },
+        {
+            title: 'Local Guardian Information',
+            content: <LocalGuardianInfo />,
+        },
+    ];
+
+    const handleStudentSubmit = async (value: any) => {
+        try {
+            console.log(value)
+        } catch (error) {
+            console.error(error);
+
+        }
+    }
+
     return (
         <div>
             <UMbreadCrumb
@@ -19,6 +54,13 @@ const CreateStuentPage = () => {
                 }
             />
 
+            <div style={{
+                padding: '24px',
+            }}>
+                <StepperForm
+                    submitHandler={(value) => handleStudentSubmit(value)}
+                    steps={steps} />
+            </div>
         </div>
     )
 }
