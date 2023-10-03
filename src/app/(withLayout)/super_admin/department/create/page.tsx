8599bac804613f39ac0c537300a34843f182ace5
AwalHossain <waqin21@gmail.com>
2023-10-04 04:08:29 +0600

    feat: Add deptApi ➕📝

--- src/app/(withLayout)/super_admin/department/create/page.tsx
+++ src/app/(withLayout)/super_admin/department/create/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import Form from "@/components/form/Form"
+import FormInput from "@/components/form/FormInput"
 import UMbreadCrumb from "@/components/ui/UMbreadCrumb"
+import { useAddDepartmentMutation } from "@/redux/api/departmentApi"
+import { Button, Col, Row, message } from "antd"
 
 const CreateDepartmentPage = () => {
     const base = 'super_admin'
+
+    const [addDepartment] = useAddDepartmentMutation()
+
+    const handleSubmit = async (value: any) => {
+        message.loading('Adding Department...')
+        try {
+            console.log(value)
+            await addDepartment(value)
+            message.success('Department Added Successfully')
+        } catch (error) {
+            console.error(error);
+
+        }
+    }
+
     return (
         <div>
 
@@ -19,9 +40,25 @@ const CreateDepartmentPage = () => {
                     ]
                 }
             />
+            <h1>Create Department</h1>
+            <br />
+            <div
+                style={{
+                    padding: "15px"
+                }}
+            >
 
-            Create Department Page
-
+                <Form submitHandler={handleSubmit}>
+                    <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                        <Col span={8} style={{ margin: "10px 0" }}>
+                            <FormInput name="title" label="Title" size="large" />
+                        </Col>
+                    </Row>
+                    <Button type="primary" htmlType="submit">
+                        Add
+                    </Button>
+                </Form>
+            </div>
         </div>
     )
 }
