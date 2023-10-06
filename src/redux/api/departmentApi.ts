fae747aa5722c4ed0c29674ef35e2b1fcad7331d
AwalHossain <waqin21@gmail.com>
2023-10-06 11:09:50 +0600

    feat: Add rtk query for manageDept 🚀

--- src/redux/api/departmentApi.ts
+++ src/redux/api/departmentApi.ts
@@ -1,3 +1,4 @@
+import { IDepartment, IMeta } from "@/types";
 import { tagTypes } from "../tag-types";
 import { baseApi } from "./baseApi";
 
@@ -5,6 +6,30 @@ const DEPARTMENT_URL = "/management-departments";
 
 export const departmentApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
+        departments: build.query({
+            query: (arg) => ({
+                url: DEPARTMENT_URL,
+                method: "GET",
+                params: arg,
+            }),
+            transformResponse: (response: IDepartment, meta: IMeta) => {
+                return {
+                    deparments: response,
+                    meta
+                }
+            },
+            providesTags: [tagTypes.department],
+        }),
+        // get single department by id
+        department: build.query({
+            query: (id) => (
+                console.log("ieeed", id),
+                {
+                    url: `${DEPARTMENT_URL}/${id}`,
+                    method: "GET",
+                }),
+            providesTags: [tagTypes.department],
+        }),
         addDepartment: build.mutation({
             query: (data) => ({
                 url: DEPARTMENT_URL,
@@ -12,8 +37,23 @@ export const departmentApi = baseApi.injectEndpoints({
                 data,
             }),
             invalidatesTags: [tagTypes.department],
+        }),
+        updateDepartment: build.mutation({
+            query: (data) => ({
+                url: `${DEPARTMENT_URL}/${data.id}`,
+                method: "PATCH",
+                data: data.body,
+            }),
+            invalidatesTags: [tagTypes.department],
+        }),
+        deleteDepartment: build.mutation({
+            query: (id) => ({
+                url: `${DEPARTMENT_URL}/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: [tagTypes.department],
         })
     }),
 })
 
-export const { useAddDepartmentMutation } = departmentApi;  
+export const { useAddDepartmentMutation, useDepartmentQuery, useDepartmentsQuery, useUpdateDepartmentMutation, useDeleteDepartmentMutation } = departmentApi;  
